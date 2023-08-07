d01b403f1ed39b0e70bbd19d3b0129a5475ddd67
Esteban <cepalacios@outlook.com>
2023-08-07 17:12:16 -0500

    test tto fix bug when populating

--- main.js
+++ main.js
@@ -1,57 +1,53 @@
-const book1 = {
-  id: 0,
-  title: 'Mind and body',
-  author: 'Alan Watts',
-};
+// DYNAMICALLY POPULATED BOOK SECTION\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+let books = [];
+const booksWrapper = document.getElementById('book-collection-wrapper');
 
-const book2 = {
-  id: 1,
-  title: 'Rich and Richness',
-  author: 'Rich marcus',
-};
+function populateSection() {
+  let books = [];
+  books = retrieveFromStorage();
+  for (let i = 0; i < books.length; i += 1) {
+    const bookElement = books[i];
 
-let books = [book1, book2];
-const booksWrapper = document.getElementById(
-  'book-collection-wrapper'
-);
-
-for (let i = 0; i < books.length; i += 1) {
-  const bookElement = books[i];
-
-  const bookHTML = `
+    const bookHTML = `
         <p>${bookElement.title}</p>
         <p>${bookElement.author}</p>
         <button>Remove</button>
         <hr />
 `;
-  const book = document.createElement('div');
-  book.innerHTML = bookHTML;
-  booksWrapper.appendChild(book);
+    const book = document.createElement('div');
+    book.innerHTML = bookHTML;
+    booksWrapper.appendChild(book);
+    books = [];
+  }
 }
-
+// LOCAL STORAGE LOGIC\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 let dataStored = [];
 
-function addBook() {
+function retrieveFromStorage() {
+  dataStored = localStorage.getItem('books');
+  return JSON.parse(dataStored);
+}
 
-  if (localStorage.getItem('books')) {
-    dataStored = localStorage.getItem('books');
-    books = JSON.parse(dataStored);
-  }
-  let book = makeBook();
-
-  books.push(book);
+function addToStorage() {
   localStorage.setItem('books', JSON.stringify(books));
 }
-const addbutton = document.querySelector('#add_button');
-addbutton.addEventListener('click', () => addBook());
 
-function makeBook () {
+function addBook() {
+  let newBook = makeBook();
+  books.push(newBook);
+  addToStorage();
+  populateSection();
+}
+const addButton = document.querySelector('#add_button');
+addButton.addEventListener('click', addBook);
+
+function makeBook() {
   const title = document.querySelector('#book_title').value;
   const author = document.querySelector('#book_author').value;
-  
+
   const book = {
     title: title,
     author: author,
-  }
+  };
   return book;
 }
