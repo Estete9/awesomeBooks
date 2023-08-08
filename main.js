d366c7f642474e79d06c9a35695d6d2b3cc33971
Esteban <cepalacios@outlook.com>
2023-08-07 19:21:56 -0500

    Functionality added: Add functionality to create the dynamically populated section with Local Storage as SSoT

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
 let books = [];
 const booksWrapper = document.getElementById('book-collection-wrapper');
-const book = document.createElement('div');
 
 function populateBookSection() {
+  let local = JSON.parse(localStorage.getItem('books'));
+
+  if (local === null) {
+    books = [];
+  } else {
+    books = local;
+  }
   for (let i = 0; i < books.length; i += 1) {
+    const book = document.createElement('div');
     const bookElement = books[i];
 
     const bookHTML = `
@@ -16,4 +23,30 @@ function populateBookSection() {
     booksWrapper.appendChild(book);
   }
 }
-window.onload = test;
+
+function addToStorage() {
+  const title = document.querySelector('#book_title').value;
+  const author = document.querySelector('#book_author').value;
+  const book = {
+    title: title,
+    author: author,
+  };
+
+  books.push(book);
+  localStorage.setItem('books', JSON.stringify(books));
+  populateBookSection();
+}
+
+const addButton = document.getElementById('add_button');
+addButton.addEventListener('click', addToStorage);
+
+window.onload = populateBookSection;
+
+// on page load
+// retrieve from local storage and update information before populating
+// call a function to populate the section with local storage data
+// add book to local storage
+// reset the current array of books
+// update the array of books from local storage
+// repopulate section
+//
