0686b8e193e1c3c9d52174f32ffcbf0adcabb80c
Esteban <cepalacios@outlook.com>
2023-08-08 08:34:29 -0500

    Finetune: Organize setup and document code

--- main.js
+++ main.js
@@ -1,17 +1,21 @@
+// VARIABLE AND ELEMENT SETUP
 let books = [];
 const booksWrapper = document.getElementById('book-collection-wrapper');
+const addButton = document.getElementById('add_button');
+addButton.addEventListener('click', addToStorage);
 
+// REPOPULATES THE BOOK SECTION LOGIC
 function populateBookSection() {
+  // retrieves from local storage to check if its empty or not, and what to do with the books array
   let local = JSON.parse(localStorage.getItem('books'));
-
   if (local === null) {
     books = [];
   } else {
     books = local;
   }
 
+  // resets the wrapper so we can create and repopulate with the updated information
   booksWrapper.innerHTML = '';
-
   for (let i = 0; i < books.length; i += 1) {
     const book = document.createElement('div');
     const bookElement = books[i];
@@ -27,29 +31,19 @@ function populateBookSection() {
   }
 }
 
+// SAVE INTERACTION WITH USER IN LOCAL STORAGE
 function addToStorage() {
+  // retrieves the user's content from the inputs and creates an object
   const title = document.querySelector('#book_title').value;
   const author = document.querySelector('#book_author').value;
   const book = {
     title: title,
     author: author,
   };
-
+  // adds the created object to local storage and restarts the section
   books.push(book);
   localStorage.setItem('books', JSON.stringify(books));
   populateBookSection();
 }
 
-const addButton = document.getElementById('add_button');
-addButton.addEventListener('click', addToStorage);
-
 window.onload = populateBookSection;
-
-// on page load
-// retrieve from local storage and update information before populating
-// call a function to populate the section with local storage data
-// add book to local storage
-// reset the current array of books
-// update the array of books from local storage
-// repopulate section
-//
