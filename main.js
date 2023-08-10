1b1d9eb9cae0479361397e1fc459a5607d35b5b9
Esteban <cepalacios@outlook.com>
2023-08-10 17:47:05 -0500

    Implement the navigation logic and styling to the selected menu btn

--- main.js
+++ main.js
@@ -1,9 +1,51 @@
 const addButton = document.getElementById('add_button');
 const booksWrapper = document.getElementById('book-collection-wrapper');
 let populateBookSection = () => {};
+const navBtns = document.querySelectorAll('#menu li');
+
 // NAVIGATION MENU LOGIC\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-
-
+// onclick receives the index of which btn was clicked
+function HighlightSelection(index) {
+  navBtns.forEach((btn, i) => {
+    btn.classList.remove('selected');
+    if (index === i) {
+      // checks which button was clicked and adds a class
+      btn.classList.add('selected');
+    }
+  });
+}
+function navigate(index) {
+  const sections = ['list', 'add-new', 'contact'];
+  const bookCollection = document.getElementById('book-collection');
+  const addBookSection = document.getElementById('add-book-section');
+  const contactSection = document.getElementById('contact-section');
+  switch (sections[index]) {
+    // compares and changes the elements class accordingly to show or hide the sections
+    case 'list': {
+      bookCollection.classList.add('show');
+      addBookSection.classList.remove('show');
+      contactSection.classList.remove('show');
+      HighlightSelection(index);
+      break;
+    }
+    case 'add-new': {
+      bookCollection.classList.remove('show');
+      addBookSection.classList.add('show');
+      contactSection.classList.remove('show');
+      HighlightSelection(index);
+      break;
+    }
+    case 'contact': {
+      bookCollection.classList.remove('show');
+      addBookSection.classList.remove('show');
+      contactSection.classList.add('show');
+      HighlightSelection(index);
+      break;
+    }
+  }
+}
+// loop through btns adding onclick
+navBtns.forEach((btn, index) => (btn.onclick = () => navigate(index)));
 // BOOK COLLECTION CLASS\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 class Books {
   constructor() {
@@ -54,7 +96,7 @@ class Books {
   attachEventListener() {
     addButton.onclick = this.addToStorage;
     const deleteBtns = Array.from(document.querySelectorAll('.delete-btn'));
-    deleteBtns.forEach((it) => it.onclick = this.deleteBook);
+    deleteBtns.forEach((it) => (it.onclick = this.deleteBook));
   }
 }
 
