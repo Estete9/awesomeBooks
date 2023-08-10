c7b750240ac07f29b2eabbd3b45286da3e9166b5
DamianDib9 <121461459+DamianDib9@users.noreply.github.com>
2023-08-10 20:52:19 -0300

    Update main.js
    "Implement functionality to navigate through pages

--- main.js
+++ main.js
@@ -14,7 +14,37 @@ function HighlightSelection(index) {
     }
   });
 }
-// CODE HERE
+
+function navigate(index) {
+  const sections = ['list', 'add-new', 'contact'];
+  const bookCollection = document.getElementById('book-collection');
+  const addBookSection = document.getElementById('add-book-section');
+  const contactSection = document.getElementById('contact-section');
+  switch (sections[index]) {
+    // compares and changes the elements class accordingly to show or hide the sections
+    case 'list': {
+      bookCollection.classList.add('show');
+      addBookSection.classList.remove('show');
+      contactSection.classList.remove('show');
+      HighlightSelection(index);
+      break;
+    }
+    case 'add-new': {
+      bookCollection.classList.remove('show');
+      addBookSection.classList.add('show');
+      contactSection.classList.remove('show');
+      HighlightSelection(index);
+      break;
+    }
+    case 'contact': {
+      bookCollection.classList.remove('show');
+      addBookSection.classList.remove('show');
+      contactSection.classList.add('show');
+      HighlightSelection(index);
+      break;
+    }
+  }
+}
 
 // loop through btns adding onclick
 navBtns.forEach((btn, index) => (btn.onclick = () => navigate(index)));
